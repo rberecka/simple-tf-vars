1a1969bc9697b344968171830629deaff37d66ad
rberecka <rachael.berecka@hashicorp.com>
2024-01-09 09:35:51 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "var-output" {
   value = <<EOT
     my_default_var: ${var.my_default_var}
     my_var_a: ${var.my_var_a}
-    my_env_var: ${var.my_env_var}
+    env_var: data.external.env.result["my_env_var"]
   EOT
 }
